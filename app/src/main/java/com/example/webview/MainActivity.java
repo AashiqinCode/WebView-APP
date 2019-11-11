2dda579ffe2241f54918d871d2fc3aa18ffdbcf5
Aashiq Ahmed M <48018540+aashiqahmed786@users.noreply.github.com>
2019-11-11 09:13:06 +0530

    Just 1

--- app/src/main/java/com/example/webview/MainActivity.java
+++ app/src/main/java/com/example/webview/MainActivity.java
@@ -1,21 +1,21 @@
 package com.example.webview;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
 
+    //This is for the function for back pressing
     @Override
     public void onBackPressed() {
         if(webView.canGoBack()){
             webView.goBack();
-        }
-        else{
+        } else {
             super.onBackPressed();
         }
 
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        webView=(WebView)findViewById(R.id.wv1);
+        webView = findViewById(R.id.wv1);
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("https://www.google.com/");
         WebSettings webSettings=webView.getSettings();
