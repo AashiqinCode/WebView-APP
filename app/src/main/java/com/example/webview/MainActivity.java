0078e1960f18946c72f465d30577e238fd4254a7
Aashiq Ahmed M <48018540+aashiqahmed786@users.noreply.github.com>
2019-11-13 13:35:28 +0530

    Set up the progress bar and changed the URL:MyBlog

--- app/src/main/java/com/example/webview/MainActivity.java
+++ app/src/main/java/com/example/webview/MainActivity.java
@@ -1,16 +1,20 @@
 package com.example.webview;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
+    ProgressBar progressBar;
 
-    //This is for the function for back pressing
+    //This is for the function for back pressing,to return to the back page on pressing the back button
     @Override
     public void onBackPressed() {
         if(webView.canGoBack()){
@@ -18,19 +22,40 @@ public class MainActivity extends AppCompatActivity {
         } else {
             super.onBackPressed();
         }
-
-
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        progressBar = findViewById(R.id.simpleProgressBar);
 
         webView = findViewById(R.id.wv1);
-        webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("https://www.google.com/");
         WebSettings webSettings=webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient() {
+
+                                     //On start of the page to load the progress bar
+                                     @Override
+                                     public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                                         super.onPageStarted(view, url, favicon);
+                                         progressBar.setVisibility(View.VISIBLE);
+                                         setTitle("Loading...");
+                                     }
+
+                                     //To stop loading the progress bar after page finish loading.
+                                     @Override
+                                     public void onPageFinished(WebView view, String url) {
+                                         super.onPageFinished(view, url);
+                                         progressBar.setVisibility(View.GONE);
+                                         setTitle(view.getTitle());
+                                     }
+                                 }
+        );
+
+        //At last load the URL
+        webView.loadUrl("http://thinkforiq.blogspot.com/");
     }
 }
+
